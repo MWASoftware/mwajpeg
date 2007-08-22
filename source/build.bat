b1f54757b453f752ac50e6a9bec7dfbb5fbb8e4b
Tony Whyman <tony.whyman@mccallumwhyman.com>
2007-08-22 22:25:44 +0000

    Fixed error in Delphi 2007 build

--- source/build.bat
+++ source/build.bat
@@ -213,7 +213,7 @@ move mwajpg.dcp Packages\D11\dclmwajpg.dcp
 "%D11%\bin\dcc32" mwajpg.dpk /DNODLL;%DEFS% /$D-,L-,Y-,R- /B /Z- /OObj /JL /UUnits\D11 /N0Units\D11 /NOUnits\D11 /NHUnits\D11 /LNPackages\D11 /LEPackages
 IF ERRORLEVEL 1 goto Quit
 del Units\D11\jpegreg2.dcu Units\D11\mwajpg.dcu
-"%D10%\bin\dcc32" source\mwadbjpg.pas /DNODLL;%DEFS% /$D-,L-,Y-,R- /JPHNE /B  /OObj  /N0Units\d11 /NHUnits\d11 /NOUnits\d11
+"%D11%\bin\dcc32" source\mwadbjpg.pas /DNODLL;%DEFS% /$D-,L-,Y-,R- /JPHNE /B  /OObj  /N0Units\d11 /NHUnits\d11 /NOUnits\d11
 IF ERRORLEVEL 1 goto Quit
 "%D11%\bin\dcc32" source\jpeglib.pas /DNODLL;CBUILDER;%DEFS% /$D-,L-,Y-,R- /JPHNE /B  /OObj  /N0. /NHUnits\d11 /NOUnits\d11
 IF ERRORLEVEL 1 goto Quit
